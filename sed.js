3481814ca1fe501b80868f48e0f0435acca6756b
zeromake <a390720046@gmail.com>
2018-05-30 14:36:05 +0800

    fix: sed

---
+++ sed.js
@@ -0,0 +1,20 @@
+const exec = require('child_process').exec;
+const os = require("os");
+
+const shell1 = ['sed', '-i']
+const shell2 = ["'s/const /var /g' dist/*.js"]
+
+function callBack(err, stdout, stderr) {
+    if (err)
+        console.error(err)
+    if (stdout)
+        console.log(stdout)
+    if (stderr)
+        console.error(stderr)
+}
+
+if (os.platform() === "darwin") {
+    exec(shell1.concat("").concat(shell2).join(" "), callBack)
+} else {
+    exec(shell1.concat(shell2).join(" "), callBack)
+}
